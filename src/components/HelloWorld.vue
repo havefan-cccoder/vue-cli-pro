43829cc25057378bb84dc30274b7095ea1ea018e
zhumingjie <zmj@enn.com>
2019-05-06 15:03:36 +0800

    vue练习

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,51 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+    <p>{{message}}</p>
+    <input type="text" name="" id="" v-model="message">
+    <p>{{objvalue.name}}</p>
+    <hr>
+    <br>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
+      <li v-for="item in list">{{item}}</li>
     </ul>
-    <h3>Essential Links</h3>
+    <hr>
+    <br>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+      <li v-for="item in list1">{{item.title}}</li>
     </ul>
-    <h3>Ecosystem</h3>
+    <hr>
+    <br>
     <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li v-for="item in list2">
+        {{item.cate}}
+        <ol>
+          <li v-for="i in item.lists">{{i.title}}</li>
+        </ol>
+      </li>
     </ul>
+    <hr>
+    <br>
+    <div v-bind:title="title">这是有title的</div>
+    <div v-bind:class="{'redclass':flag}">这是一个红色div</div>
+    <ul>
+      <li v-for="(item, key) in list" v-bind:class="{'redclass': key==0, 'blueclass': key==1}">{{item}}</li>
+    </ul>
+    <div class="box" v-bind:style="{'width': widthvalue}">
+      这是一块
+    </div>
+    <hr>
+    <br>
+    <button v-on:click="getMsg()">获取</button>
+    <button v-on:click="setMsg()">设置</button>
+    <hr>
+    <br>
+    <input type="text" ref="userinfo" />
+    <div ref="box">box</div>
+    <button v-on:click="getInputValue()">获取input值，并改变div颜色</button>
+    <hr>
+    <br>
+    <button data-aid="133" @click="eventFn($event)">事件获取自定义属性值</button>
   </div>
 </template>
 
@@ -35,6 +54,57 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data () {
+    return {
+      message:'哈哈',
+      objvalue: {
+        name: '张三'
+      },
+      list: [1, 2, 3],
+      list1: [
+        { title: 111 },
+        { title: 222 },
+        { title: 333 }
+      ],
+      list2: [
+        {
+          cate: '国内新闻',
+          lists: [
+            { title: 'aaa' },
+            { title: 'bbb' },
+            { title: 'ccc' }
+          ]
+        },
+        {
+          cate: '国外新闻',
+          lists: [
+            { title: 'AAA' },
+            { title: 'BBB' },
+            { title: 'CCC' }
+          ]
+        }
+      ],
+      flag: true,
+      title: '这是title',
+      widthvalue: '300px'
+    }
+  },
+  methods: {
+    getMsg() {
+      alert(this.message)
+    },
+    setMsg() {
+      this.message = '我是Vue'
+    },
+    getInputValue() {
+      alert(this.$refs.userinfo.value);
+      this.$refs.box.style.background = "red";
+    },
+    eventFn(e) {
+        console.log(e);
+        alert(e.srcElement.dataset.aid);
+    }
   }
 }
 </script>
@@ -54,5 +124,15 @@ li {
 }
 a {
   color: #42b983;
+}
+.redclass{
+  color: red;
+}
+.blueclass{
+  color: blue;
+}
+.box{
+  height: 100px;
+  background: #42b983;
 }
 </style>
