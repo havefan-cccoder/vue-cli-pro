4b7437338c9802f933ae8a9c978257df07af1d78
zhumingjie <zmj@enn.com>
2019-05-10 11:08:18 +0800

    分数取两位小数

--- src/views/calculator.vue
+++ src/views/calculator.vue
@@ -9,7 +9,7 @@
         <label>{{num2}}</label>
         =
         <button class="box" v-if="!tureanswer" @click="showAnswer()">?</button>
-        <button v-if="tureanswer" class="reusltbox">{{sumval}}</button>
+        <button v-if="tureanswer" class="reusltbox">{{sumvvalue}}</button>
         <button v-if="tureanswer" class="rightanswer"  @click="chooseResult('y')">√</button>
         <button v-if="tureanswer" class="wronganswer"  @click="chooseResult('n')">×</button>
     </div>
@@ -26,6 +26,7 @@ export default {
             num2: 2,
             tureanswer: false,
             wrongs: 0,
+            sumvvalue: 0
         }
     },
     // mounted () {
@@ -38,19 +39,29 @@ export default {
     },
     methods: {
         chooseResult(val) {
-            this.num1 = Math.floor(Math.random()*1000);
-            this.num2 = Math.floor(Math.random()*1000);
             this.tureanswer = false;
             this.rounds++;
-            if (val === 'n') {
+            if (val === 'n' && this.sumvvalue === this.sumval ||
+                val === 'y' && this.sumvvalue != this.sumval) {
                 this.wrongs++;
                 this.scores = (this.rounds - this.wrongs)/this.rounds*100;
             }else{
                 this.scores = (this.rounds - this.wrongs)/this.rounds*100;
             }
+            var NUM = new Number(this.scores);
+            this.scores = NUM.toFixed(2);
+            //必须先计算分数再随机生成要计算的数，不然computed会被更改
+            this.num1 = Math.floor(Math.random()*1000);
+            this.num2 = Math.floor(Math.random()*1000);
         },
         showAnswer() {
             this.tureanswer = true;
+            var num = Math.floor(Math.random()*10);
+            if (this.rounds % num === 0) {
+                this.sumvvalue = this.sumval;
+            }else{
+                this.sumvvalue = Math.floor(Math.random()*1000);
+            }
         }
     },
     filters: {
