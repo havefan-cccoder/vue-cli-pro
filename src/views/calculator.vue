7fa6852838cee382e4bac4f8a26debe02a45e9fa
zhumingjie <zmj@enn.com>
2019-05-10 11:22:26 +0800

    重置按钮

--- src/views/calculator.vue
+++ src/views/calculator.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="outerbox">
         <p>计算方法名称组件？：加减乘除</p>
-        <div>
-            Round:{{rounds}}&nbsp;&nbsp;Scores:{{scores}}%&nbsp;&nbsp;Wrong:{{wrongs}}
+        <div class="countbox">
+            Round:{{rounds}}&nbsp;&nbsp;Scores:{{scores | getPercent}}%&nbsp;&nbsp;Wrong:{{wrongs}}
         </div>
         <label >{{num1}}</label>
         +
@@ -12,6 +12,8 @@
         <button v-if="tureanswer" class="reusltbox">{{sumvvalue}}</button>
         <button v-if="tureanswer" class="rightanswer"  @click="chooseResult('y')">√</button>
         <button v-if="tureanswer" class="wronganswer"  @click="chooseResult('n')">×</button>
+        <br>
+        <button class="resetbtn" @click="resetData()">重置</button>
     </div>
 </template>
 
@@ -22,15 +24,16 @@ export default {
         return {
             scores:0,
             rounds:0,
-            num1: 1,
-            num2: 2,
+            num1: Math.floor(Math.random()*1000),
+            num2: Math.floor(Math.random()*1000),
             tureanswer: false,
             wrongs: 0,
             sumvvalue: 0
         }
     },
     // mounted () {
-        
+    //     this.num1 = Math.floor(Math.random()*1000);
+    //     this.num2 = Math.floor(Math.random()*1000);
     // },
     computed: {
         sumval: function() {
@@ -48,8 +51,8 @@ export default {
             }else{
                 this.scores = (this.rounds - this.wrongs)/this.rounds*100;
             }
-            var NUM = new Number(this.scores);
-            this.scores = NUM.toFixed(2);
+            // var NUM = new Number(this.scores);
+            // this.scores = NUM.toFixed(2);
             //必须先计算分数再随机生成要计算的数，不然computed会被更改
             this.num1 = Math.floor(Math.random()*1000);
             this.num2 = Math.floor(Math.random()*1000);
@@ -62,17 +65,30 @@ export default {
             }else{
                 this.sumvvalue = Math.floor(Math.random()*1000);
             }
+        },
+        resetData() {
+            this.scores = 0;
+            this.rounds = 0;
+            this.num1 =  Math.floor(Math.random()*1000);
+            this.num2 =  Math.floor(Math.random()*1000);
+            this.tureanswer =  false;
+            this.wrongs =  0;
+            this.sumvvalue =  0;
         }
     },
     filters: {
-        getPercent(oval, nval) {
-            
+        getPercent(oval) { // 分数取两位小数
+            var NUM = new Number(oval);
+            return NUM.toFixed(2)
         }
     }
 }
 </script>
 
 <style scoped>
+.countbox{
+    margin-bottom: 10px;
+}
 .outerbox{
     width: 320px;
     margin: 0 auto;
@@ -114,6 +130,13 @@ button{
     display: inline-block;
     background: #ddd;
     cursor: inherit;
+}
+.resetbtn{
+    padding: 5px 10px;
+    background: #407ccf;
+    color: #fff;
+    border-radius: 5px;
+    font-size: 14px;
 }
 </style>
 
